acc16fa20051dc6de76e1593c1753488bee6560b
PhuHuy123 <nguyenphuhuy180920@gmail.com>
2023-01-10 13:24:15 +0700

    backend API

---
+++ Backend/src/controllers/authorController.js
@@ -0,0 +1,61 @@
+const { Author, Article } = require("../model/model");
+const authorController = {
+  //ADD AUTHOR
+  addAuthor: async (req, res) => {
+    try {
+      const newAuthor = new Author(req.body);
+      const savedAuthor = await newAuthor.save();
+      res.json({
+        status: 200,
+        data: savedAuthor,
+        message: "Tạo mới thành công",
+      });
+    } catch (err) {
+      res.status(500).json(err); //HTTP REQUEST CODE
+    }
+  },
+
+//   //GET ALL AUTHORS
+  getAllAuthors: async (req, res) => {
+    try {
+      const authors = await Author.find();
+      res.status(200).json(authors);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+//   //GET AN AUTHOR
+  getAnAuthor: async (req, res) => {
+    try {
+      const author = await Author.findById(req.params.id).populate("articles");
+      res.status(200).json(author);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+//   //UPDATE AUTHOR
+  updateAuthor: async (req, res) => {
+    try {
+      const author = await Author.findById(req.params.id);
+      await author.updateOne({ $set: req.body });
+      res.status(200).json("Updated successfully!");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+//   //DELETE AUTHOR
+  deleteAuthor: async (req, res) => {
+    try {
+      await Article.updateMany({ author: req.params.id }, { author: null });
+      await Author.findByIdAndDelete(req.params.id);
+      res.status(200).json("Deleted successfully!");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
+
+module.exports = authorController;
